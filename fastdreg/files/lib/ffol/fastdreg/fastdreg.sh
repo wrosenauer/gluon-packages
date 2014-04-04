18cf5a606a540f8031acfb1d9acc6134a73c7911
bioxz <patrick.uven@informatik.uni-oldenburg.de>
2014-04-05 01:15:58 +0200

    git status

--- fastdreg/files/lib/ffol/fastdreg/fastdreg.sh
+++ fastdreg/files/lib/ffol/fastdreg/fastdreg.sh
@@ -9,7 +9,7 @@ pubkey=$(/etc/init.d/fastd show_key mesh_vpn)
 
 regdone=$(uci get fastdreg.ffol.regdone)
 
-if [ ! $regdone ]; then
+if [ $regdone ]; then
 	reg=$(wget -T15 "$keysrv/reg.php?name=$hostname&key=$pubkey" -O -)
 	if [ "$reg" == "regdone" ]; then
 		uci set fastdreg.ffol.regdone=1
