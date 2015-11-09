2f57457665e2466965e0209183d3633564e20ac1
Wolfgang Rosenauer <wr@rosenauer.org>
2015-11-10 00:17:44 +0100

    fastdreg.sh verbessert (early exit) und an FFAN API angepasst

--- fastdreg/files/lib/ffol/fastdreg/fastdreg.sh
+++ fastdreg/files/lib/ffol/fastdreg/fastdreg.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 . /lib/functions.sh
 
-keysrv="http://freifunk-ansbach.de"
-
-hostname=$(uci get system.@system[0].hostname)
-
-pubkey=$(/etc/init.d/fastd show_key mesh_vpn)
+# FIXME: site.conf or site.mk ???
+keysrv="https://site.freifunk-neuendettelsau.de"
 
 regdone=$(uci get fastdreg.ffol.regdone)
+if [ "$regdone" -eq "1" ] ; then exit 0 ; fi
+vpn=$(uci get fastd.mesh_vpn.enabled)
+if [ "$regdone" -ne "1" ] ; then exit 0 ; fi
 
-if [ "$regdone" -ne "1" ]; then
-	reg=$(wget -T15 "$keysrv/reg.php?name=$hostname&key=$pubkey" -O -)
-	if [ "$reg" == "regdone" ]; then
-		uci set fastdreg.ffol.regdone=1
-		uci commit
-	fi
+hostname=$(uci get system.@system[0].hostname)
+pubkey=$(/etc/init.d/fastd show_key mesh_vpn)
+
+reg=$(wget -T15 "$keysrv/router-anmelden/node.php?hostname=$hostname&key=$pubkey" -O -)
+if [ "$reg" == "regdone" ]; then
+	uci set fastdreg.ffol.regdone=1
+	uci commit
 fi
+
+exit 0
